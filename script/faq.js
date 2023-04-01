53fecd803dd8c7dd342c4699125b5f70adcc44d0
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-04-01 14:53:55 +0530

    faq box

--- script/faq.js
+++ script/faq.js
@@ -2,13 +2,18 @@ var box=document.getElementById("faq-side-but");
 var status="hidden";
 box.addEventListener("click",()=>{
     if(status=="hidden"){
-        document.getElementById("faq-box").style=" grid-template-rows: 30% 70%;height:50%; transition:height 4s";
-        document.getElementById("faq-ans").style="display:flex;transition:display 4s";
-        status="visibile"
+        document.getElementById("faq-box").style=" grid-template-rows: 30% 70%;height:40%; transition:height 1s";
+        window.setTimeout(()=>{
+            document.getElementById("faq-ans").style="display:flex;";
+        },300);
+       
+        status="visibile";
     }
     else if(status=="visibile"){
-        document.getElementById("faq-box").style=" grid-template-rows: 100% 0%;height:20%; transition:height 4s";
-        document.getElementById("faq-ans").style="display:none;transition:display 4s";
+        document.getElementById("faq-box").style=" grid-template-rows: 100% 0%;height:20%; transition:height 1s";
+      
+            document.getElementById("faq-ans").style="display:none";
+     
         status="hidden";
     }
 
