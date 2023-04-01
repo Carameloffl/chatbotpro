2acd14c77750ab074adaff72cc9fd18016050d40
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-04-01 20:10:39 +0530

    faq page completed and bot page design modified

--- script/faq.js
+++ script/faq.js
@@ -1,21 +1,19 @@
-var box=document.getElementById("faq-side-but");
 var status="hidden";
-box.addEventListener("click",()=>{
+function cal(a){
+  
     if(status=="hidden"){
-        document.getElementById("faq-box").style=" grid-template-rows: 30% 70%;height:40%; transition:height 1s";
+        document.getElementById("faq-box"+a).style=" grid-template-rows: 30% 70%;height:38%; transition:height 1s";
         window.setTimeout(()=>{
-            document.getElementById("faq-ans").style="display:flex;";
+            document.getElementById("faq-ans"+a).style="display:flex;";
         },300);
        
         status="visibile";
     }
     else if(status=="visibile"){
-        document.getElementById("faq-box").style=" grid-template-rows: 100% 0%;height:20%; transition:height 1s";
+        document.getElementById("faq-box"+a).style=" grid-template-rows: 100% 0%;height:20%; transition:height 1s";
       
-            document.getElementById("faq-ans").style="display:none";
+            document.getElementById("faq-ans"+a).style="display:none";
      
         status="hidden";
     }
-
-})
-console.log(box);
+}
